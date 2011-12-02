799a9e0d38c1536da5680d4ac7194ca7b9ca636d
unknown <libart2@LIB-ART-4XBFMG1.home.ku.edu>
2011-12-02 13:34:20 -0600

    First commit.

---
+++ Family.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:         Family
+# Purpose:      This contains the code to create and manage families.
+#
+# Author:      Nick Birney
+#
+# Created:     2 Dec. 2011
+# Copyright:   (c) Nick Birney 2011
+# Licence:     GNU General Public License (see text file named COPYING)
+#-------------------------------------------------------------------------------
+
+class familyMember:
+    name = ""
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
